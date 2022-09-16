3230902e2613bf9aa6532c63fe717c2f55590a5a
Theodore Whitehead <theodore.whitehead@arup.com>
2022-09-16 15:46:17 +0930

    Layout and some functionality added

---
+++ Arup Renamer.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Arup_Renamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBox_files = new System.Windows.Forms.CheckedListBox();
+            this.textBox_FolderPath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_OpenFile = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.buttonSelectAll = new System.Windows.Forms.Button();
+            this.buttonDeselectAll = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.buttonAppend = new System.Windows.Forms.Button();
+            this.buttonPrepend = new System.Windows.Forms.Button();
+            this.buttonApply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBox_files
+            // 
+            this.checkedListBox_files.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox_files.CheckOnClick = true;
+            this.checkedListBox_files.FormattingEnabled = true;
+            this.checkedListBox_files.Location = new System.Drawing.Point(6, 137);
+            this.checkedListBox_files.Name = "checkedListBox_files";
+            this.checkedListBox_files.ScrollAlwaysVisible = true;
+            this.checkedListBox_files.Size = new System.Drawing.Size(504, 364);
+            this.checkedListBox_files.Sorted = true;
+            this.checkedListBox_files.TabIndex = 0;
+            this.checkedListBox_files.UseTabStops = false;
+            // 
+            // textBox_FolderPath
+            // 
+            this.textBox_FolderPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_FolderPath.Location = new System.Drawing.Point(69, 517);
+            this.textBox_FolderPath.Name = "textBox_FolderPath";
+            this.textBox_FolderPath.Size = new System.Drawing.Size(441, 20);
+            this.textBox_FolderPath.TabIndex = 1;
+            this.textBox_FolderPath.Text = "C:\\";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_OpenFile
+            // 
+            this.button_OpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button_OpenFile.Location = new System.Drawing.Point(6, 516);
+            this.button_OpenFile.Name = "button_OpenFile";
+            this.button_OpenFile.Size = new System.Drawing.Size(57, 23);
+            this.button_OpenFile.TabIndex = 2;
+            this.button_OpenFile.Text = "Open";
+            this.button_OpenFile.UseVisualStyleBackColor = true;
+            this.button_OpenFile.Click += new System.EventHandler(this.button_OpenFile_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // buttonSelectAll
+            // 
+            this.buttonSelectAll.Location = new System.Drawing.Point(6, 111);
+            this.buttonSelectAll.Name = "buttonSelectAll";
+            this.buttonSelectAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelectAll.TabIndex = 3;
+            this.buttonSelectAll.Text = "Select All";
+            this.buttonSelectAll.UseVisualStyleBackColor = true;
+            this.buttonSelectAll.Click += new System.EventHandler(this.buttonSelectAll_Click);
+            // 
+            // buttonDeselectAll
+            // 
+            this.buttonDeselectAll.Location = new System.Drawing.Point(85, 111);
+            this.buttonDeselectAll.Name = "buttonDeselectAll";
+            this.buttonDeselectAll.Size = new System.Drawing.Size(75, 23);
+            this.buttonDeselectAll.TabIndex = 4;
+            this.buttonDeselectAll.Text = "Deselect All";
+            this.buttonDeselectAll.UseVisualStyleBackColor = true;
+            this.buttonDeselectAll.Click += new System.EventHandler(this.buttonDeselectAll_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(167, 114);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(81, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Multi-Select";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(6, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(3, 31);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(35, 13);
+            this.labelName.TabIndex = 7;
+            this.labelName.Text = "Name";
+            // 
+            // buttonAppend
+            // 
+            this.buttonAppend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonAppend.Location = new System.Drawing.Point(446, 44);
+            this.buttonAppend.Name = "buttonAppend";
+            this.buttonAppend.Size = new System.Drawing.Size(58, 23);
+            this.buttonAppend.TabIndex = 8;
+            this.buttonAppend.Text = "Append";
+            this.buttonAppend.UseVisualStyleBackColor = true;
+            // 
+            // buttonPrepend
+            // 
+            this.buttonPrepend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPrepend.Location = new System.Drawing.Point(382, 44);
+            this.buttonPrepend.Name = "buttonPrepend";
+            this.buttonPrepend.Size = new System.Drawing.Size(58, 23);
+            this.buttonPrepend.TabIndex = 9;
+            this.buttonPrepend.Text = "Prepend";
+            this.buttonPrepend.UseVisualStyleBackColor = true;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonApply.Location = new System.Drawing.Point(6, 554);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(504, 23);
+            this.buttonApply.TabIndex = 10;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 589);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.buttonPrepend);
+            this.Controls.Add(this.buttonAppend);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.buttonDeselectAll);
+            this.Controls.Add(this.buttonSelectAll);
+            this.Controls.Add(this.button_OpenFile);
+            this.Controls.Add(this.textBox_FolderPath);
+            this.Controls.Add(this.checkedListBox_files);
+            this.Name = "Form1";
+            this.Text = "Arup Renamer";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBox_files;
+        private System.Windows.Forms.TextBox textBox_FolderPath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_OpenFile;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button buttonDeselectAll;
+        private System.Windows.Forms.Button buttonSelectAll;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonPrepend;
+        private System.Windows.Forms.Button buttonAppend;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonApply;
+    }
+}
+
